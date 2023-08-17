ca77a02a44941ccc1c594bcad94bfec2a08a0ded
Mufil Rahman A <ramanizum@gmail.com>
2023-08-17 20:14:17 +0530

    children

--- study.js
+++ study.js
@@ -4,3 +4,19 @@ const itemList= document.querySelector('#items');
 
 //parentNode
 console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor='#f4f4';
+console.log(itemList.parentNode.parentNode);
+
+//parent element 
+
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor='#f4f4f4';
+console.log(itemList.parentElement.parentElement);
+
+//childNode -> it include spacing aswell - not much use
+console.log(itemList.childNodes);
+
+//children
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor='yellow';
