772f1b2c82ac9530a3106728d75f1e57f9e7241b
Mufil Rahman A <ramanizum@gmail.com>
2023-08-17 19:33:43 +0530

    QuerySelector && QuerySelectorALL

--- main.js
+++ main.js
@@ -1,85 +1,98 @@
-//examin the document object 
-console.dir(document);
+// //examin the document object 
+// console.dir(document);
 
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-console.log(typeof (title1));
-document.title=1234;
-console.log(document.doctype);
-console.log(document.head);
-console.log(typeof document.head);
-console.log(document.all);
-console.log(document.all[10]);
-// document.all[10].textContent='Hello';  -> not a goodway 
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(typeof (title1));
+// document.title=1234;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(typeof document.head);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent='Hello';  -> not a goodway 
 
-console.log(document.forms);
-console.log(document.links);
+// console.log(document.forms);
+// console.log(document.links);
 
 
-const headerT= document.getElementById('header-title');
-const header= document.getElementById('main-header');
-console.log(headerT);
-// headerT.textContent='I am textContent';-- not consider style
-// headerT.innerText='I am innertext'; --> consider the style
-// Text content only returns the text value and not the HTML tags
-// textContent gets the content of all elements, including <script> and <style> elements
-// innerText returns only the text content and no HTML tags
-// innerText not gets the content of all elements, including <script> and <style> elements
- headerT.innerHTML='I am innerHTML';
- header.style.border='solid 3px black';
+// const headerT= document.getElementById('header-title');
+// const header= document.getElementById('main-header');
+// console.log(headerT);
+// // headerT.textContent='I am textContent';-- not consider style
+// // headerT.innerText='I am innertext'; --> consider the style
+// // Text content only returns the text value and not the HTML tags
+// // textContent gets the content of all elements, including <script> and <style> elements
+// // innerText returns only the text content and no HTML tags
+// // innerText not gets the content of all elements, including <script> and <style> elements
+//  headerT.innerHTML='I am innerHTML';
+//  header.style.border='solid 3px black';
 
- //get elements by class name 
+//  //get elements by class name 
 
- const items=document.getElementsByClassName('list-group-item');
+//  const items=document.getElementsByClassName('list-group-item');
  
-//  items[1].textContent='hello  2';
-//  items[1].style.fontWeight='bold';
-//  items[1].style.backgroundColor='yellow';
+// //  items[1].textContent='hello  2';
+// //  items[1].style.fontWeight='bold';
+// //  items[1].style.backgroundColor='yellow';
 
- for(const value of items){
-    value.style.backgroundColor='#f4f4';
- }
+//  for(const value of items){
+//     value.style.backgroundColor='#f4f4';
+//  }
 
- //get element by tag name 
+//  //get element by tag name 
 
- const tags = document.getElementsByTagName('li');
- console.log(tags);
- for(const value of tags){
-    value.style.fontWeight='bold';
- }
+//  const tags = document.getElementsByTagName('li');
+//  console.log(tags);
+//  for(const value of tags){
+//     value.style.fontWeight='bold';
+//  }
 
- const input = document.querySelector('input');
- input.value='Hello your world';
+//  const input = document.querySelector('input');
+//  input.value='Hello your world';
 
- const inputsubmit = document.querySelector('input[type="submit"]');
- console.log(inputsubmit);
- inputsubmit.style.backgroundColor='green';
- inputsubmit.classList.add('mt-3');
+//  const inputsubmit = document.querySelector('input[type="submit"]');
+//  console.log(inputsubmit);
+//  inputsubmit.style.backgroundColor='green';
+//  inputsubmit.classList.add('mt-3');
 
 
-  //getting last item 
+//   //getting last item 
 
-  lastItem= document.querySelector('.list-group-item:last-child');
-  console.log(lastItem);
-  lastItem.innerHTML="Last child";
+//   lastItem= document.querySelector('.list-group-item:last-child');
+//   console.log(lastItem);
+//   lastItem.innerHTML="Last child";
 
-  secondItem= document.querySelector('.list-group-item:nth-child(2)');
-  console.log(secondItem);
-  secondItem.innerHTML="Second Child"
+//   secondItem= document.querySelector('.list-group-item:nth-child(2)');
+//   console.log(secondItem);
+//   secondItem.innerHTML="Second Child"
 
-  //select odd items 
+//   //select odd items 
 
-  const odditems= document.querySelectorAll('li:nth-child(odd)');
+//   const odditems= document.querySelectorAll('li:nth-child(odd)');
+//   console.log(odditems);
+//   for(const value of odditems){
+//     value.style.color='red';
+//   }
+
+
+//   //sharpner task 
+
+//   const itemadded= document.querySelector('#newli');
+//   console.log(itemadded);
+//   itemadded.innerHTML='created';
+  
+secondItem= document.querySelector('.list-group-item:nth-child(2)');
+thirdItem= document.querySelector('.list-group-item:nth-child(3)');
+
+secondItem.style.backgroundColor='green';
+thirdItem.innerText='';
+
+const odditems= document.querySelectorAll('li:nth-child(odd)');
+const allitems=document.querySelectorAll('li');
+allitems[1].style.color='green';
   console.log(odditems);
   for(const value of odditems){
-    value.style.color='red';
+    value.style.backgroundColor='green';
   }
-
-
-  //sharpner task 
-
-  const itemadded= document.querySelector('#newli');
-  console.log(itemadded);
-  itemadded.innerHTML='created';
-  
